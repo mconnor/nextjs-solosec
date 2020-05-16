3a3b7dfb8a4a29bec2f79825856eede83400ae97
mike connor <mike@rocketnumber9.org>
2020-05-15 21:09:39 -0500

    remove next.config

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Head from 'next/head';
 import GlobalStyle from './utils/GobalStyle';
+import style from 'styled-components'
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -9,6 +10,19 @@ type Props = {
     children: ReactNode;
     title?: string;
 }
+const PageContainer = style.div`
+    background-color: white;
+    width: var(--ipad-max-width);
+    display: grid;
+    grid-template-columns: 6% 1fr 6%;
+    grid-template-rows: var(--header-height) var(--main-height) var(--footer-height);
+    grid-template-areas:
+        "header header header"
+        "main main main"
+        ". footer .";
+    justify-items: stretch;
+`;
+
 
 const Layout: React.FC<Props> = ({ children, title = 'This is the default title' })=> (
 		<>
@@ -18,11 +32,11 @@ const Layout: React.FC<Props> = ({ children, title = 'This is the default title'
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
             <GlobalStyle/>
-			<div className='page-container'>
+			<PageContainer>
 				<Header gridArea='header'/>
 				<Main gridArea='main' children={children} />
 				<Footer gridArea='footer'/>
-			</div>
+			</PageContainer>
 		</>
 	);
 
