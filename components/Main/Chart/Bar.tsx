212e55195627522fac6b9c2b2f31b7bc199ab897
mike connor <mike@rocketnumber9.org>
2020-05-25 13:35:53 -0500

    bar chart animation working

--- components/Main/Chart/Bar.tsx
+++ components/Main/Chart/Bar.tsx
@@ -1,11 +1,28 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-const BarDiv = styled.div<Props>`
+
+const variants = {
+    start: {
+        scaleY: 0,
+        transition: {
+            delay: .2
+        }
+    },
+    end: {
+        scaleY: 1
+    }
+}
+
+
+
+const BarDiv = styled(motion.div)<Props>`
     position: relative;
     width: 128px;
     background: ${props => props.placebo ? 'gray' : 'green'};
     height: ${prop => `${prop.max}px`};
     text-align:center;
+    transform-origin: bottom;
 `;
 
 
@@ -27,7 +44,17 @@ type Props = {
 
 const Bar: React.FC<Props> = ({ max, placebo, score}) => {
     return (
-        <BarDiv placebo={placebo} max={max} score={score}>
+        <BarDiv placebo={placebo} max={max} score={score}
+            variants={variants}
+            initial='start'
+            animate='end'
+            transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+                delay:.5
+            }}
+        >
             <Score>{score}%</Score>
         </BarDiv>
     )
--- components/Main/Chart/index.tsx
+++ components/Main/Chart/index.tsx
@@ -1,8 +1,9 @@
 
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Bar from './Bar';
 
-const MainDiv = styled.div<Props>`
+const MainDiv = styled(motion.div)<Props>`
 
     /* border: 1px solid; */
     position: relative;
@@ -14,7 +15,7 @@ const MainDiv = styled.div<Props>`
     grid-template-columns: 128px 128px;
     column-gap: 31px;
     align-items: end;
-
+  
 `;
 
 type Props = {
@@ -28,9 +29,29 @@ type Props = {
     
 }
 
+
+const containerVariants = {
+    start: {
+        opacity:0,
+        transition: {
+            staggerChildren: 3,
+            delayChildren: 2,
+            staggerDirection: -1,
+            when: "afterChildren"
+        }
+    },
+    end: { opacity:1 },
+  };
+
+
 const Chart:React.FC<Props> = ({ chartTop=273, chartL=334 , chartH=247, max1=164, max2=40, score1=67.7, score2=17.7 }) => {
+
     return (
-        <MainDiv chartTop={chartTop} chartL={chartL} chartH={chartH}>
+        <MainDiv chartTop={chartTop} chartL={chartL} chartH={chartH}
+            variants={containerVariants}
+            initial='start'
+            animate='end'
+        >
             <Bar  max={max1} score={score1} />
             <Bar placebo max={max2} score={score2} />
         </MainDiv>
