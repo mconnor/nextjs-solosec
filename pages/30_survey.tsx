78fdf29068befc3b0d72e11cd004688015006c2c
mike connor <mike@rocketnumber9.org>
2020-05-27 13:30:08 -0500

    pg30 svg anim fix

--- pages/30_survey.tsx
+++ pages/30_survey.tsx
@@ -2,9 +2,11 @@ import Layout from '../components/Layout';
 import * as Sections from '../components/utils/Sections';
 
 export default () => (
-    <Layout 
-        bgArt='/img/svg/pg30.svg' 
+    <Layout
+        bgArt='/img/svg/pg30.svg'
         section={Sections.SURVEY_30}>
-        <div><img src="/img/svg/pg30-women.svg" alt="survey" /></div>
+ 
+            <object type="image/svg+xml" data="/img/svg/pg30-women.svg" />
+    
     </Layout>
 )
