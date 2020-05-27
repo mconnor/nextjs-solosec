c5c88b5ea3932817d860784e8978631dac9fc636
Chad Specter <chad.specter@gmail.com>
2020-05-27 10:55:50 -0400

    Added animation on survey page

--- pages/30_survey.tsx
+++ pages/30_survey.tsx
@@ -5,5 +5,6 @@ export default () => (
     <Layout 
         bgArt='/img/svg/pg30.svg' 
         section={Sections.SURVEY_30}>
+        <div><img src="/img/svg/pg30-women.svg" alt="survey" /></div>
     </Layout>
 )
