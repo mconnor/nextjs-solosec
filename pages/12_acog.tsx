fc4e31c8caa617ea6888eb3261e551a5ea5f73a0
Chad Specter <chad.specter@gmail.com>
2020-05-27 11:30:45 -0400

    CHecks page 12

--- pages/12_acog.tsx
+++ pages/12_acog.tsx
@@ -4,7 +4,7 @@ import * as Sections from '../components/utils/Sections'
 
 
 export default () => (
-    <Layout noBgArt={true} section={Sections.ACOG_12}>
-        <img src="/img/svg/pg12.svg" className='fillMain' />
+    <Layout bgArt="/img/svg/pg12.svg" section={Sections.ACOG_12}>
+        <div><img src="/img/svg/pg12-checks.svg" alt="survey" /></div>
     </Layout>
 )
