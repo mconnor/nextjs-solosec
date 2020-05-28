4d677144c3f465eabcf91213735283cea1d04683
mike connor <mike@rocketnumber9.org>
2020-05-27 22:30:26 -0500

    pg 71 QA

--- components/Main/QA/index.tsx
+++ components/Main/QA/index.tsx
@@ -1,38 +1,126 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
-import QnA from './QnA'
+import { motion } from 'framer-motion';
+// import { useAppState } from '../../../state'
+import { useToggle } from '../../../hooks';
+// import QnA from './QnA'
+
+
+
+const variants = {
+    open: { 
+        y: 0,
+        opacity:1, 
+        display: 'block' 
+    },
+    closed: {
+        y: "-100%",
+        opacity: 0,
+        display: 'none',
+        transition: {
+            delay: .2
+        }
+    },
+
+}
 
 const MainDiv = styled.div`
-    
-    margin-left: 93px;
-    margin-right: 93px;
-margin-top: 90px;
+    height:600px;
+    margin-left: 98px;
+    margin-right: 127px;
+    margin-top: 90px;
 
     display: grid;
     grid-template-columns: 1fr;
-    gap: 70px;
+    grid-template-rows: repeat(6,auto);
+    /* gap: 70px; */
 `;
 
 
-
 type Props = {
-    qaArray: string[][];
+    children: ReactNode;
 }
 
-const QA: React.FC<Props> = ({ qaArray }) => {
+const QA: React.FC<Props> = ({ children }) => {
+  
     return (
         <MainDiv>
-            {
-                qaArray.map((arrayItem: string[], i: number) => {
-                    return (
-                        <QnA
-                            q={arrayItem[0]}
-                            a={arrayItem[1]}
-                            key={i} />
-                    )
-                })
-            }
+            {children}
         </MainDiv>
     )
 }
 
 export default QA
+
+
+
+
+const Qdiv = styled.div`
+    font-style:bold;
+`;
+
+type OnOff = { on?: boolean; }
+
+const Adiv = styled(motion.div)<OnOff>`
+    margin-top: 22px;
+    /* padding: 0 18px; */
+    /* display: ${props => props.on ? 'block' : 'none'}; */
+    /* overflow: hidden; */
+    font-style: normal;
+    font-weight: 300;
+    font-size: 22px;
+    line-height: 25px;
+    letter-spacing: -0.035em;
+`;
+
+const QAcontainer = styled.div`
+
+    color:var(--button-selected-color);
+    font-style: normal;
+    font-weight: normal;
+    font-size: 26px;
+    line-height: 31px;
+    letter-spacing: -0.02em;
+`;
+
+interface Iprops {
+    q: string;
+    a: string;
+}
+
+
+export const QuestionAnswer: React.FC<Iprops> = ({ q, a }) => {
+    const { isToggled, toggle } = useToggle(false);
+    // const { toggleQA, isaQAopen, setQA} =useAppState();
+
+
+    return (
+        <QAcontainer onClick={toggle}>
+            <Qdiv role='button'
+                dangerouslySetInnerHTML={createMarkup(q)} />
+           
+                
+                <Adiv 
+                    // on={isToggled}
+                    dangerouslySetInnerHTML={createMarkup(a)}
+                    
+                    variants={variants}
+                    initial='closed'
+                    animate={isToggled ? 'open' : 'closed'}
+                    transition={{ damping: 300 }}
+                    // initial={{ opacity: 0 }}
+                    // animate={{ opacity: 1 }}
+                    // exit={{ opacity: 0, display: 'none'}}
+                >
+                </Adiv>
+      
+        </QAcontainer>
+    )
+}
+
+
+
+
+function createMarkup(copy: string) {
+    return { __html: copy }
+}
--- pages/71_qa.tsx
+++ pages/71_qa.tsx
@@ -1,73 +1,82 @@
 
 
 import Layout from '../components/Layout';
-import QA from '../components/Main/QA'
+import QA, { QuestionAnswer } from '../components/Main/QA'
 import * as Sections from '../components/utils/Sections';
 
+export default () => {
 
-
-const QARRAY = [
-    ['<section>\
-    How often does the patient take Solosec® (secnidazole)?</section>',
-   
-        '<p>\
-        Solosec does not dissolve in water or any liquids. It should not be mixed into liquids and \
-        should only be taken with the recommended foods: applesauce, pudding, or yogurt.<sup>1</sup> \
-        The entire contents of the Solosec packet should be sprinkled onto applesauce, yogurt, or pudding and consumed once within 30 minutes without chewing or crunching the granules.1 Instruct patients that they should not try to dissolve Solosec in water or any other liquids. However, they are free to drink a glass of water after taking their Solosec dose to aid in swallowing </p>'
-        ],
-   
-   
-    ['<section>How should I instruct my patient to take Solosec?</section>',
-   
-        '<p>The entire contents of the Solosec packet should be sprinkled onto applesauce, yogurt, or pudding and consumed once within 30 minutes without chewing or crunching the granules.1 Instruct patients that they should not try to dissolve Solosec in water or any other liquids. However, they are free to drink a glass of water after taking their Solosec dose to aid in swallowing</p>'
-    ],
-    [
-    '<section> Are there any lifestyle changes I should advise my patient to make during treatment with Solosec?</section>',
-        '<p>\
-        There is no specific food or alcohol restriction while being treated with Solosec.<sup>1</sup> \
-        However, adult patients should always consume alcohol in a responsible manner. \
-        </p>\
-        <p>\
-        In vitro drug alcohol \
-        studies show Solosec does not inhibit the enzyme that metabolizes alcohol.<sup>1</sup> \
-        </p>'
-    ],
-    [
-    '<section>What potential AEs should I watch out for in my patients taking Solosec?</section>',
-        '<p>\
-        Solosec is generally well tolerated. The most common side effects include yeast infection, headache, \
-        nausea, vomiting, diarrhea, abdominal pain, vaginal itching, and an unpleasant metallic taste in the mouth.<sup>1</sup> \
-        </p>\
-        <p>\
-        Please see Important Safety Information below and the accompanying full Prescribing Information. \
-        </p>\
-        <p>\
-        Report suspected side effects of prescription drugs to the FDA at \
-        <a href="www.fda.gov/MedWatch">www.fda.gov/MedWatch</a> \
-        or 1-800-FDA-1088, \
-        or contact Lupin Pharmaceuticals, Inc. at 1-844-SOLOSEC (1-844-765-6732). \
-        </p>'
-    ], [
-        '<section>Which patients should NOT take Solosec?</section>',
-       ' <p>xxxx</p>'
-    ],
-    [
-       ' <section>How can my patients access the savings card?</section>',
-      '  <p>xxxx</p>'
-    ]
-
-   ];
-
-
-
-export default () =>{ 
-    console.log('QARRAY');
-    // console.log(QARRAY);
     return (
-    <Layout 
-        bgArt='/img/svg/pg71-BG.svg'
-        section={Sections.BV_68}
-    >
-       <QA qaArray={QARRAY} />
-    </Layout>
-)}
+        <Layout
+            bgArt='/img/svg/pg71-BG.svg'
+            section={Sections.BV_68}
+        >
+            <QA>
+                <QuestionAnswer
+                    q='How often does the patient take Solosec&circledR; (secnidazole)'
+                    a='<p>
+                        Solosec does not dissolve in water or any liquids. It should not be mixed into liquids and
+                        should only be taken with the recommended foods: applesauce, pudding, or yogurt.&sup1;
+                        The entire contents of the Solosec packet should be sprinkled onto applesauce, yogurt, or pudding and consumed once within
+                        30 minutes without chewing or crunching the granules.1 Instruct patients that they should not try to dissolve Solosec in water or any other liquids.
+                        However, they are free to drink a glass of water after taking their Solosec dose to aid in swallowing
+                    </p>'
+                />
+                <QuestionAnswer
+                    q='How should I instruct my patient to take Solosec?'
+                    a='<p>
+                        The entire contents of the Solosec packet should be sprinkled onto applesauce, yogurt, or pudding and consumed once
+                        within 30 minutes without chewing or crunching the granules.1 Instruct patients that they should not try 
+                        to dissolve Solosec in water or any other liquids. However, they are free to drink 
+                        a glass of water after taking their Solosec dose to aid in swallowing
+                     </p>'
+                />
+                <QuestionAnswer
+                    q='Are there any lifestyle changes I should advise my patient to make during treatment with Solosec?'
+                    a='<p>
+                            There is no specific food or alcohol restriction while being treated with Solosec.&sup1;
+                            However, adult patients should always consume alcohol in a responsible manner.
+                        </p>
+                        <p>
+                            In vitro drug alcohol
+                            studies show Solosec does not inhibit the enzyme that metabolizes alcohol.&sup1;
+                        </p>'
+                />
+                <QuestionAnswer
+                    q='What potential AEs should I watch out for in my patients taking Solosec?'
+                    a='<p>
+                        Solosec is generally well tolerated. The most common side effects include yeast infection, headache,
+                        nausea, vomiting, diarrhea, abdominal pain, vaginal itching, and an unpleasant metallic taste in the mouth.&sup1;
+                    </p>
+                    <p>
+                        Please see Important Safety Information below and the accompanying full Prescribing Information.
+                    </p>
+                    <p>
+                        Report suspected side effects of prescription drugs to the FDA at
+                        <a href="www.fda.gov/MedWatch">www.fda.gov/MedWatch</a>
+                        or 1-800-FDA-1088,
+                        or contact Lupin Pharmaceuticals, Inc. at 1-844-SOLOSEC (1-844-765-6732).
+                    </p>'
+                />
+                <QuestionAnswer
+                    q='Which patients should NOT take Solosec?'
+                    a='<p>
+                    Patients who have a history of hypersensitivity to secnidazole, other ingredients in the formulation,
+                     or other nitroimidazole derivatives should not take Solosec.&sup1; Additionally, breastfeeding patients 
+                     should be advised to stop breastfeeding for 96 hours after administration of Solosec and feed stored milk or formula.
+                     If pumping, the patient should discard the milk. If patients choose to continue breastfeeding, they should not take Solosec. 
+                        </p>'
+                />
+                <QuestionAnswer
+                    q='How can my patients access the savings card?'
+                    a='<p>
+                    Patients can visit solosec.com/savings-card to access a card to download or send to mobile. 
+                    You may also request physical cards form your Lupin representative.
+                     The physical card can be used in the pharmacy, or patients may scan its QR code to download the 
+                     co-pay card offer to their mobile phone. No pre-registration or pre-authorization is required. 
+                    </p>'
+                />
+            </QA>
+        </Layout>
+    )
+}
