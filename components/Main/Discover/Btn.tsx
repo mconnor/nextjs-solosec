009dcac27d95f52c2eb3f6963158027fbd7643b4
Chad Specter <chad.specter@gmail.com>
2020-05-28 11:02:04 -0400

    Working on nav

--- components/Main/Discover/Btn.tsx
+++ components/Main/Discover/Btn.tsx
@@ -1,10 +1,13 @@
 
 import { useState } from 'react';
 import styled from 'styled-components';
+import {linkTo} from "../../modals/Nav/PageList";
 
 type Props = {
     svglink:string;
     svgClicked: string;
+    page:string;
+    seq:string;
 }
 
 const Img = styled.img`
@@ -13,11 +16,11 @@ height: 201px;
 `;
 
 
-const Btn:React.FC<Props> = ({ svglink, svgClicked}) => {
+const Btn:React.FC<Props> = ({ svglink, svgClicked, page, seq}) => {
     const [showSVG, setShowSVG] = useState(`/img/svg/${svglink}.svg`);
     return (
         <>
-             <Img src={showSVG} onClick={() => setShowSVG(`/img/svg/${svgClicked}.svg`)} />
+             <Img src={showSVG} onClick={() => {setShowSVG(`/img/svg/${svgClicked}.svg`); linkTo(page, true, seq)}} />
         </>
     )
 }
