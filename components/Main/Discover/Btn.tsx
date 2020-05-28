ea9352f9da7bb226cad13c8a7e06a82fed42957c
mike connor <mike@rocketnumber9.org>
2020-05-28 14:19:33 -0500

    typescript …useEffect

--- components/Main/Discover/Btn.tsx
+++ components/Main/Discover/Btn.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import {useAppState} from "../../../state";
 import {PageList} from "../../modals/Nav/PageList";
@@ -13,7 +13,7 @@ type Props = {
 
 const Img = styled.img`
     width: 460px;
-height: 201px;
+    height: 201px;
 `;
 
 
@@ -21,17 +21,26 @@ const Btn:React.FC<Props> = ({ svglink, svgClicked, page, seq}) => {
     const [showSVG, setShowSVG] = useState(`/img/svg/${svglink}.svg`);
     const {currSeq, setCurrentSequence} = useAppState();
 
-    function linkTo(page:string, seq:string) {
-        if (setCurrentSequence) {
-            setCurrentSequence(seq);
+    const [linkUrl] = useState<string>(PageList["pages"][page])
+
+    useEffect(() => {
+        setCurrentSequence && setCurrentSequence(seq);
+        console.log(page, seq, currSeq);
+        
+    },[page, seq, currSeq,setCurrentSequence])
+
+    function linkTo(page:string, linkUrl:string) {
+        
             console.log(page, seq, currSeq);
-            setTimeout(()=>{window.location.href = PageList["pages"][page];}, 100)
-        }
+            setTimeout(()=>{window.location.href = linkUrl;}, 100)
+       
     }
 
     return (
         <>
-             <Img src={showSVG} onClick={() => {setShowSVG(`/img/svg/${svgClicked}.svg`); linkTo(page, seq)}} />
+             <Img src={showSVG} onClick={() => {
+                 setShowSVG(`/img/svg/${svgClicked}.svg`);
+                 linkTo(page, linkUrl)}} />
         </>
     )
 }
