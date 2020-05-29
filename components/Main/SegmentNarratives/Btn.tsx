bfde1905909266ff73bd3b965cf30e34194d8e97
mike connor <mike@rocketnumber9.org>
2020-05-29 08:50:12 -0500

    recreate SegmentNarratives
    from master

---
+++ components/Main/SegmentNarratives/Btn.tsx
@@ -0,0 +1,48 @@
+
+// import { useState } from 'react';
+import styled from 'styled-components';
+import {PageList} from "../../modals/Nav/PageList";
+import { useCookie } from '../../../hooks'
+import { useRouter } from 'next/router'
+
+
+type Props = {
+    page:string;
+    seq:string
+}
+
+const Button = styled.img`
+    width: 360px;
+    height: 75px;
+    background: transparent;
+    border: none !important;
+    font-size:0;
+`;
+
+
+const Btn:React.FC<Props> = ({ page, seq}) => {
+    const [cookie, setCookie] = useCookie({ key: "seq" }) ;
+    const router = useRouter()
+
+    //const [linkUrl] = useState<string>(PageList["pages"][page])
+
+    // useEffect(() => {
+    //     setCurrentSequence && setCurrentSequence(seq);
+    //     currSeq && setCookie(currSeq);
+    // },[showSVG])
+
+    function linkTo(page:string) {
+        console.log(cookie);
+        setCookie(seq)
+        setTimeout(()=>{router.push("/" + PageList.pages[page]);}, 100)
+
+    }
+
+    return (
+        <Button onClick={() => {
+            linkTo(page)}}>
+        </Button>
+    )
+}
+
+export default Btn
