81322e3fab5aa5904d60479d4764cba186dc584d
Chad Specter <chad.specter@gmail.com>
2020-05-29 07:34:34 -0400

    Reset navigation if selected a navigation item from the nav menu

--- components/Header/NavBtn.tsx
+++ components/Header/NavBtn.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import styled from 'styled-components'
+import {useCookie} from "../../hooks";
 
 interface StyleProps  {
     borderRight?: boolean;
@@ -25,8 +26,11 @@ interface Props extends StyleProps, ChildrenProps {}
 
 
 const NavBtn: React.FC<Props> = ({ children, borderRight, clickCallBack }) => {
+    const [cookie, setCookie] = useCookie({ key: "seq" }) ;
+    console.log(cookie);
+
     return (
-        <Btn role='button'  borderRight={borderRight} onClick={() => clickCallBack && clickCallBack()} >
+        <Btn role='button'  borderRight={borderRight} onClick={() => setCookie("") && clickCallBack && clickCallBack()} >
             {children}
         </Btn>
     )
