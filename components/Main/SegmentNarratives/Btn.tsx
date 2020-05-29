8e5a39712b6f781eaf6bd4028adce738a5c155f4
mike connor <mike@rocketnumber9.org>
2020-05-29 10:55:24 -0500

    fix messed up merge

--- components/Main/SegmentNarratives/Btn.tsx
+++ components/Main/SegmentNarratives/Btn.tsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import styled from 'styled-components';
 import {PageList} from "../../modals/Nav/PageList";
 import { useCookie } from '../../../hooks'
 import Link from 'next/link'
-=======
-
-// import { useState } from 'react';
-import styled from 'styled-components';
-import {PageList} from "../../modals/Nav/PageList";
-import { useCookie } from '../../../hooks'
-import { useRouter } from 'next/router'
->>>>>>> origin/master
 
 
 type Props = {
@@ -18,11 +9,7 @@ type Props = {
     seq:string
 }
 
-<<<<<<< HEAD
 const BtnInvisible = styled.button`
-=======
-const Button = styled.img`
->>>>>>> origin/master
     width: 360px;
     height: 75px;
     background: transparent;
@@ -33,18 +20,13 @@ const Button = styled.img`
 
 const Btn:React.FC<Props> = ({ page, seq}) => {
     const [cookie, setCookie] = useCookie({ key: "seq" }) ;
-<<<<<<< HEAD
 
 console.log('cookie', cookie) // keep this
 
 
-    const url =  PageList.pages[page].slice(0,-5);
-   
-
+    const url =  PageList.pages[page];
 
     return (
-  
-
 
         <Link href={url}>
             <BtnInvisible onClick={() =>{ 
@@ -54,29 +36,4 @@ console.log('cookie', cookie) // keep this
 }
 
 export default Btn
-=======
-    const router = useRouter()
 
-    //const [linkUrl] = useState<string>(PageList["pages"][page])
-
-    // useEffect(() => {
-    //     setCurrentSequence && setCurrentSequence(seq);
-    //     currSeq && setCookie(currSeq);
-    // },[showSVG])
-
-    function linkTo(page:string) {
-        console.log(cookie);
-        setCookie(seq)
-        setTimeout(()=>{router.push("/" + PageList.pages[page]);}, 100)
-
-    }
-
-    return (
-        <Button onClick={() => {
-            linkTo(page)}}>
-        </Button>
-    )
-}
-
-export default Btn
->>>>>>> origin/master
--- components/Main/SegmentNarratives/index.tsx
+++ components/Main/SegmentNarratives/index.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
 import styled from 'styled-components';
 import Btn from './Btn';
 
@@ -37,9 +32,6 @@ const SegmentNarratives: React.FC<Props> = () => {
     )
 }
 
-<<<<<<< HEAD
+
 export default SegmentNarratives
 
-=======
-export default SegmentNarratives
->>>>>>> origin/master
