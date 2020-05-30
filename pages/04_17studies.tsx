7b068c467645f5a75e647e55d48a04b85840b762
mike connor <mike@rocketnumber9.org>
2020-05-30 16:47:37 -0500

    fix study bg art

--- pages/04_17studies.tsx
+++ pages/04_17studies.tsx
@@ -1,10 +1,11 @@
 
 import Layout from '../components/Layout';
-import Studies from '../components/Main/Studies'
 import { STUDIES_17 } from '../components/utils/Sections'
 
 export default () => (
-    <Layout noBgArt={true} section={STUDIES_17} pageIndex={3}>
-        <Studies svg='/img/svg/pg17.svg'></Studies>
-</Layout>
+    <Layout
+        bgArt='/img/svg/pg17.svg'
+        section={STUDIES_17}
+        pageIndex={3}
+    />
 )
