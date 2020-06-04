e846432ef1d2ed81d6cfec73f38e02b2fb94dd55
Chad Specter <chad.specter@gmail.com>
2020-06-03 22:10:39 -0400

    Switch cookie to memorystorage
    Signed-off-by: Chad Specter <chad.specter@gmail.com>

--- hooks/useCookie.ts
+++ hooks/useCookie.ts
@@ -1,19 +1,21 @@
-import { Dispatch, SetStateAction, useState, useEffect } from "react";
-import Cookies from "js-cookie";
-
+import {Dispatch, SetStateAction, useState, useEffect} from "react";
+//import Cookies from "js-cookie";
+const MemoryStorage = require('memorystorage');
+// here, the MemoryStorage function is available
+const store = new MemoryStorage();
 
 type Props = {
     key: string;
 }
 
-export const useCookie = ({ key }: Props): [string, Dispatch<SetStateAction<string>>] => {
+export const useCookie = ({key}: Props): [string, Dispatch<SetStateAction<string>>] => {
     //const initial = Cookies.get(key) ?? '';
-    const initial = typeof window !== 'undefined' && localStorage.getItem(key) || '';
+    const initial = store.getItem(key) || '';
     const [cookie, setCookie] = useState(initial);
 
     useEffect(() => {
         //Cookie.set(key, cookie);
-        typeof window !== 'undefined' && localStorage.setItem(key, cookie);
+        store.setItem(key, cookie);
     }, [cookie, key]);
 
     return [cookie, setCookie]
